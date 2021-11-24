fe452acd2e46dc19c54bf966758b46a1cbab60c0
codepanther <pezechigozirim@gmail.com>
2021-11-25 00:50:20 +0100

    Refactor components to use hooks

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,14 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable no-useless-constructor */
 
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Button extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { name, classes, onClickHandler } = this.props;
-    return (
-      <div className={classes}>
-        <button type="button" onClick={onClickHandler}>{name}</button>
-      </div>
-    );
-  }
-}
+const Button = ({ name, classes, onClickHandler }) => (
+  <div className={classes}>
+    <button type="button" onClick={onClickHandler}>{name}</button>
+  </div>
+);
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,

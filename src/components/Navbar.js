90b03c2590e05ab55346adf173bdbc1a462672cd
codepanther <pezechigozirim@gmail.com>
2021-11-30 23:21:20 +0100

    Make the app resposive on all screen sizes
    Add mobile navigation menu
    
    Add simple animations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import { RiCloseLine, RiMenu2Line } from 'react-icons/ri';
 
-const Navbar = () => (
-  <div className="navbar__container pa4">
-    <div className="navbar__container-logo">
-      <p className="f3 b">Math Magicians</p>
-    </div>
-    <div className="navbar__container-navlinks flex">
-      <Link to="/" className="no_underline white">
-        <p className="navbar__container-navlinks_link">
-          Home
-        </p>
-      </Link>
-      <Link to="/calculator" className="no_underline white">
-        <p className="navbar__container-navlinks_link">
-          Calculator
-        </p>
-      </Link>
-      <Link to="/quote" className="no_underline white">
-        <p className="navbar__container-navlinks_link">
-          Quote
-        </p>
-      </Link>
-    </div>
-  </div>
+const Menu = () => (
+  <>
+    <Link to="/" className="no_underline white">
+      <p className="navbar__container-navlinks_link">Home</p>
+    </Link>
+    <Link to="/calculator" className="no_underline white">
+      <p className="navbar__container-navlinks_link">Calculator</p>
+    </Link>
+    <Link to="/quote" className="no_underline white">
+      <p className="navbar__container-navlinks_link">Quote</p>
+    </Link>
+  </>
 );
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="navbar__container pa4">
+      <div className="navbar__container-logo">
+        <p className="f3 b">Math Magicians</p>
+      </div>
+      <div className="navbar__container-navlinks flex">
+        <Menu />
+      </div>
+      <div className="navbar__container-menu">
+        {toggleMenu ? (
+          <RiCloseLine onClick={() => setToggleMenu(false)} />
+        ) : (
+          <RiMenu2Line onClick={() => setToggleMenu(true)} />
+        )}
+        {
+          toggleMenu && (
+            <div className="navbar__container-menu_items shadow-pop-tr">
+              <Menu />
+            </div>
+          )
+        }
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
